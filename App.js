257b8146d96063386fec43186837459632923be0
durgesh <durgesh31g@gmail.com>
2024-02-23 15:25:45 +0530

    Added Parcel to build the project

--- App.js
+++ App.js
@@ -1,6 +1,25 @@
-const heading = React.createElement('h1', {id:'heading'},'Hello World');
+import React from "react";
+import ReactDom from "react-dom/client";
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World Update this"
+);
 console.log(heading);
-const root = ReactDOM.createRoot(document.getElementById("rot"));
 
-root.render(heading);
+const heading2 = <h1>Test</h1>;
 
+const Heading3 = () => {
+  return (
+    <>
+      <div>
+        <h1> Added the function component</h1>
+      </div>
+    </>
+  );
+};
+
+const root = ReactDom.createRoot(document.getElementById("rot"));
+
+root.render(<Heading3></Heading3>);
